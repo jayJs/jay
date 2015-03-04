999ca5cc04f5e2d5548eaa4ac420acbe18f1bb84
Martin Sookael <m@451.ee>
2015-03-04 17:32:57 +0200

    added .jGallery support

--- jay.js
+++ jay.js
@@ -250,4 +250,47 @@ function put(table, id, data) {
       window[tag.id] = $("#"+tag.id);
     }
   }
+
+  // jGallery
+  $(".jGallery").each(function(one){
+
+    var current = $(this);
+    current.prepend("<div id='jFull'></div>");
+
+    var images = $('.jGallery').find('img').map(function() {
+      $(this).out();
+      return this;
+    }).get();
+
+    $(".jGallery").append("<div id='jThumbs'></div>");
+
+    $.each(images, function( index, value ) {
+      $("#jThumbs").append("<div style='background-image:url("+value.src+");' class='jThumb'>");
+    });
+
+    current.find('.jThumb').each(function(){
+      $(this).on('click', function(){
+        var thisImg = $(this);
+
+        // make thumb active
+        current.find('.jThumb').each(function(){
+          if(thisImg.attr("style") === $(this).attr("style")){
+            $(this).addClass("active");
+
+            var bg = $(this).css('background-image');
+            bg = bg.replace('url(','').replace(')','');
+
+            $("#jFull").empty().append("<img src='"+bg+"'>");
+
+          } else {
+            $(this).removeClass("active");
+          }
+        });
+      });
+    });
+    $( ".jThumb:first" ).css( "margin-left", "0" );
+    $( ".jThumb:last" ).css( "margin-right", "0" );
+    $( ".jThumb:first" ).trigger( "click" );
+  });
+
 }( jQuery ));
